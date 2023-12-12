a68e0e4a4aa6693b6660683dc99f12d7b7e646f5
deepod <davidapitt@gmail.com>
2023-12-13 00:30:02 +0100

    Second commit

--- src/Quiz.java
+++ src/Quiz.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class Quiz {
 
     Scanner sc = new Scanner(System.in);
-    public static Question[] questions = new Question[3];
-    public static int points = 0;
+    public Question[] questions = new Question[3];
+    public int points = 0;
 
     public Quiz(){
         questions[0] = new Question("Who was the first computer programmer in history?", "Ada Lovelace");
         questions[1] = new Question("What is the capital city of Canada?", "Ottawa");
         questions[2] = new Question("Which planet is known as the Red Planet?", "Mars");
     }
-
-
 
     public void start() {
         for (int i = 0; i < questions.length; i++) {
@@ -31,15 +29,10 @@ public class Quiz {
             System.out.println("Incorrect");
             System.out.println("Corect answer: " + question.answerText);
         }
-
     }
 
     public void outputResults(){
         System.out.println("Total points: " + points);
         System.out.println("Goodbye!");
     }
-
-
-
-
 }
