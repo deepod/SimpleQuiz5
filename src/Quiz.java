cbef83b698ea9cc966e674e9f110b77d348859cb
deepod <davidapitt@gmail.com>
2023-12-24 10:27:58 +0100

    third commit - comments added

--- src/Quiz.java
+++ src/Quiz.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class Quiz {
 
     Scanner sc = new Scanner(System.in);
-    public Question[] questions = new Question[3];
-    public int points = 0;
+    public Question[] questions = new Question[3]; // an array to hold 3 questionsayer's points
+    public int points = 0; // to hold the pl
 
-    public Quiz(){
+    // this block of code fills the array with questions when the program starts
+    {
         questions[0] = new Question("Who was the first computer programmer in history?", "Ada Lovelace");
         questions[1] = new Question("What is the capital city of Canada?", "Ottawa");
         questions[2] = new Question("Which planet is known as the Red Planet?", "Mars");
     }
 
+    /**
+     * asks each question in the array
+     */
     public void start() {
         for (int i = 0; i < questions.length; i++) {
             ask(questions[i]);
